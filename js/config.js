e190a543ece6f08b51c2fe07434e124f1f43488b
Dan <3181113162@qq.com>
2025-12-30 16:31:43 +0800

    两个版本不同配置

--- js/config.js
+++ js/config.js
@@ -1,11 +1,26 @@
-export const API_BASE = 'http://127.17.0.1/api/v1/chats';
-export const API_TOKEN = 'ragflow-qkFLtyQSa7vfBTMP0tSeGqxAm3FwQ9b6EuCzvJJUGyA';
+//v nightly
+// export const API_BASE = 'http://127.17.0.1/api/v1/chats';
+// export const API_TOKEN = 'ragflow-qkFLtyQSa7vfBTMP0tSeGqxAm3FwQ9b6EuCzvJJUGyA';
+// export const MAX_DEBATE_ROUNDS = 5;
+
+// export const AGENTS = {
+//     general: { id: 'c9fe7e74de3e11f0b40873186f897dd2', name: '通用知识', class: 'c-general', icon: 'fa-brain', roleText: 't-general', sessionId: null },
+//     geophysical: { id: '63a0d520dbe211f087b8ee91a51e2da3', name: '物探专家', class: 'c-geo', icon: 'fa-magnet', roleText: 't-geo', sessionId: null },
+//     geochemical: { id: '09304acadbed11f087b8ee91a51e2da3', name: '化探专家', class: 'c-chem', icon: 'fa-flask', roleText: 't-chem', sessionId: null },
+//     achievement: { id: '0daab9aadbed11f087b8ee91a51e2da3', name: '成果分析', class: 'c-achieve', icon: 'fa-chart-line', roleText: 't-achieve', sessionId: null },
+//     host: { id: '03a24a42e54911f09e2ae5a769207490', name: '主持人', class: 'c-host', icon: 'fa-user-tie', roleText: 't-host', sessionId: null }
+// };
+
+
+// v0.22.1
+export const API_BASE = 'http://172.17.0.1:88/api/v1/chats';
+export const API_TOKEN = 'ragflow-Zk1KipZvDHTu7pS6GXQy5As-4bbvujpQPcM66nSdVDs';
 export const MAX_DEBATE_ROUNDS = 5;
 
 export const AGENTS = {
-    general: { id: 'c9fe7e74de3e11f0b40873186f897dd2', name: '通用知识', class: 'c-general', icon: 'fa-brain', roleText: 't-general', sessionId: null },
+    general: { id: 'bb1df310dbe111f087b8ee91a51e2da3', name: '通用知识', class: 'c-general', icon: 'fa-brain', roleText: 't-general', sessionId: null },
     geophysical: { id: '63a0d520dbe211f087b8ee91a51e2da3', name: '物探专家', class: 'c-geo', icon: 'fa-magnet', roleText: 't-geo', sessionId: null },
     geochemical: { id: '09304acadbed11f087b8ee91a51e2da3', name: '化探专家', class: 'c-chem', icon: 'fa-flask', roleText: 't-chem', sessionId: null },
     achievement: { id: '0daab9aadbed11f087b8ee91a51e2da3', name: '成果分析', class: 'c-achieve', icon: 'fa-chart-line', roleText: 't-achieve', sessionId: null },
-    host: { id: '03a24a42e54911f09e2ae5a769207490', name: '主持人', class: 'c-host', icon: 'fa-user-tie', roleText: 't-host', sessionId: null }
+    host: { id: '14a5f60cdbed11f087b8ee91a51e2da3', name: '主持人', class: 'c-host', icon: 'fa-user-tie', roleText: 't-host', sessionId: null }
 };
